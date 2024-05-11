79b91dc3bf7d10ba93fddd29c9f58a45da631f20
KhaledMahfouz5 <khaledmhfz2004@gmail.com>
2024-05-11 15:35:53 +0000

    Type Annotations With Array #06 	modified:   index.ts

--- index.ts
+++ index.ts
@@ -1,33 +1,15 @@
 /*
-  Type Annotations || Signature
-  -- Indicate The Data Type Of Variables
-  -- Indicate The Data Type Of Functions Input/Output
-  -- Objects, etc.
-
-  - Why We Use It ?
-  - Is It Mandatory ?
-  - What Happen If We Didnt Use It ?
+  Type Annotations With Arrays
 */
 
-let theName = "Elzero";
-let theAge: number = 40;
-let hire: boolean = true;
-let all: any = "Elzero Web School";
-let allVars; // Any
+let all: string | number | boolean = "Osama";
 
-theName = "Osama";
+all = "A";
 all = 100;
+all = true;
 
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let myFriends: string[] = ["Osama", "Ahmed", "Sayed"];
+
+for (let i = 0; i < myFriends.length; i++) {
+  console.log(myFriends[i].repeat(3));
 }
-
-console.log(add(10, 20));
-console.log(typeof add(10, 20));
-
-function add(n1, n2) {
-  return n1 + n2;
-}
-
-console.log(add(10, "20"));
-console.log(typeof add(10, "20"));
