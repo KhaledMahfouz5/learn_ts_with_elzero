f348bef55feed371a6d04acb3da73d764e931db4
KhaledMahfouz5 <khaledmhfz2004@gmail.com>
2024-05-11 15:39:13 +0000

    Type Annotations With Function 	modified:   index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 /*
-  Type Annotations With Multidimensional Arrays
+  Type Annotations With Functions
+
+  - noImplicitAny
+
+  - noImplicitReturns
+  --- Will Check All Code Paths In A Function To Ensure They Return A Value
+
+  - noUnusedLocals
+  --- Report Errors On Unused Local Variables
+
+  - noUnusedParameters
+  --- Report Errors On Unused Parameters In Functions.
 */
 
-let arrayOne: number[] = [1, 2, 3, 4, 5];
-let arrayTwo: string[] = [A, B, C];
-let arrayThree: (string | number)[] = [1, 2, 3, 4, A, B, C];
+let showMsg = true;
 
-let arrayFour: (string | number | string[] | boolean)[] = [1, 2, 3, 4, A, B, [C, D], true, false];
+function showDetails(name: string, age: number, salary: number) : string {
+  let test = 10;
+  if (showMsg) {
+    return `Hello ${name}, Age Is ${age}, Salary Is ${salary}, Test Variable ${test}`;
+  }
+  return `No Data To Show`;
+}
+
+console.log(showDetails("Osama", 40, 5000));
